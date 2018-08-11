2fb0d2869bc3b7075fef2facd66e3bd88e4bf41f
wvorac <39820472+wvorac@users.noreply.github.com>
2018-08-11 10:27:41 -0700

    Update app-new.py

--- app-new.py
+++ app-new.py
@@ -13,13 +13,13 @@ import en_core_web_sm
 import matplotlib
 
 matplotlib.use("Agg")
-from config import consumer_key, consumer_secret, access_token, access_token_secret
+# from config import consumer_key, consumer_secret, access_token, access_token_secret
 
 # Get config variable from environment variables
-#consumer_key = os.environ.get("consumer_key")
-#consumer_secret = os.environ.get("consumer_secret")
-#access_token = os.environ.get("access_token")
-#access_token_secret = os.environ.get("access_token_secret")
+consumer_key = os.environ.get("consumer_key")
+consumer_secret = os.environ.get("consumer_secret")
+access_token = os.environ.get("access_token")
+access_token_secret = os.environ.get("access_token_secret")
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
